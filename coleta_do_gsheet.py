2f8e6411c9b02faa2f05ee2744e3c1a1a04f35f7
fredprada9 <fredprada9@gmail.com>
2023-01-31 23:42:37 -0300

    substituindo arquivo de coleta .ipynb por .py

---
+++ coleta_do_gsheet.py
@@ -0,0 +1,13 @@
+pip install streamlit
+pip install gsheetsdb
+
+import streamlit as st
+import pandas as pd
+from gsheetsdb import connect
+
+st.title("Connect to Google Sheets")
+gsheet_url = st.secrets["public_gsheets_url"]
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+df_gsheet = pd.DataFrame(rows)
+st.write(df_gsheet)
