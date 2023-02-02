4f1ca94bbedfa78143c76e4901a7c0e41fe53819
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2023-02-01 22:42:38 -0300

    Update coleta_do_gsheet.py

--- coleta_do_gsheet.py
+++ coleta_do_gsheet.py
@@ -3,9 +3,9 @@ import pandas as pd
 from gsheetsdb import connect
 
 st.title("Connect to Google Sheets")
-#gsheet_url = st.secrets["public_gsheets_url"]
-gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/edit?usp=sharing"
-conn = connect()
-rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-df_gsheet = pd.DataFrame(rows)
+# gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/edit?usp=sharing"
+# conn = connect()
+# rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+# df_gsheet = pd.DataFrame(rows)
+df_gsheet = print('teste')
 st.write(df_gsheet)
