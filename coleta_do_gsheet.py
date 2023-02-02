9b9c505b79a403bf5d48dfb2f7986418a21bf31d
fredprada9 <fredprada9@gmail.com>
2023-02-01 23:45:31 -0300

    correção do arquivo de coleta

--- coleta_do_gsheet.py
+++ coleta_do_gsheet.py
@@ -1,11 +1,22 @@
 import streamlit as st
 import pandas as pd
 from gsheetsdb import connect
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 st.title("Connect to Google Sheets")
-#gsheet_url = st.secrets["public_gsheets_url"]
-gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/edit?usp=sharing"
-conn = connect()
-rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-df_gsheet = pd.DataFrame(rows)
-st.write(df_gsheet)
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = "C:/Users/Fred/OneDrive/Documentos/FREDERICO/Frederico/6 python_dev/streamlit/credentials.json"
+creds = ServiceAccountCredentials.from_json_keyfile_name(credentials, scope)
+client = gspread.authorize(creds)
+
+# Acesse a planilha do Google Sheets pelo nome ou id da planilha
+sheet = client.open("dataset_compras_gsheets")
+worksheet = sheet.get_worksheet(0) # Seleciona a primeira aba
+data = worksheet.get_all_values()
+
+df = pd.DataFrame(data)
+
+st.write(df)
