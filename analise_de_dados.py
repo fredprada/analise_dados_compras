c8e3cb338e9a757de75747dbcf0589447cfff7fa
fredprada9 <fredprada9@gmail.com>
2023-02-02 00:57:50 -0300

    teste streamlit

--- analise_de_dados.py
+++ analise_de_dados.py
@@ -1,12 +1,10 @@
-pip install plotly
-
 from coleta_de_dados import dados_tratados
 import streamlit as st
 # import numpy as np
 # import scipy
 # from scipy.stats import shapiro
-# import matplotlib.pyplot as plt
-import plotly.express as px
+import matplotlib.pyplot as plt
+# import plotly.express as px
 
 st.title("Apresentando histogramas")
 
@@ -14,6 +12,11 @@ st.title("Apresentando histogramas")
 df = dados_tratados()
 
 # histograma dos clientes
-histograma_clientes = px.histogram(df, x="clientes")
 st.write("**Histograma dos clientes**")
-st.plotly_chart(histograma_clientes)
+
+plt.figure(figsize=(15,10))
+plt.hist(df['clientes'], bins=20)
+# plt.title("Histograma de Pacientes cadastrados")
+plt.xlabel("Quantidade")
+
+st.pyplot()
