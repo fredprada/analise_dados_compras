accdfc8d1ee25021edad20646a51ec1293d1559f
fredprada9 <fredprada9@gmail.com>
2023-02-02 00:34:27 -0300

    adicionando arquivo de tratamento de dados e fazendo teste de histogramas

---
+++ analise_de_dados.py
@@ -0,0 +1,23 @@
+from coleta_de_dados import dados_tratados
+import streamlit as st
+import numpy as np
+import scipy
+from scipy.stats import shapiro
+import matplotlib.pyplot as plt
+import plotly
+from ggplot import *
+
+# coletando os dados tratados
+df = dados_tratados()
+
+st.title("Apresentando histogramas")
+# Criando um histograma
+p = ggplot(df, aes(x='clientes')) + geom_histogram(binwidth=1)
+
+c = ggplot(df, aes(x='compras')) + geom_histogram(binwidth=1)
+
+f = ggplot(df, aes(x='fornecedores')) + geom_histogram(binwidth=1)
+
+st.write(p)
+st.write(c)
+st.write(f)
--- coleta_tratamento_de_dados.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("Connect to Google Sheets")
-
-arquivo_base = "dataset_compras.xlsx - Sheet1.csv"
-df = pd.read_csv(arquivo_base)
-
-st.write(df)
