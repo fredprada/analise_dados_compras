0578aa8ec80f695eeeeafbb0c8114e81f347ad73
fredprada9 <fredprada9@gmail.com>
2023-02-05 02:21:09 -0300

    mudando o rumo da apresentação das métricas e colocando .describe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,27 @@ import streamlit as st
 import seaborn as sns
 import plotly.express as px
 
+# defining page settings
 st.set_page_config(
     layout="wide",  
     page_title="EDA compras")
 
+# importing treated data and storing in pandas dataframe for later use
+df_treated_data = treated_data()
+
+# introducing context for the analysis
 st.title("Análise de dados de compras")
 st.header("Entendendo o comportamento dos clientes, compras e fornecedores de uma empresa de varejo.")
 st.write("""A análise tem como objetivo principal 
                 entender as relações entre as três variáveis
                 a fim de saber qual delas tem maior
                 impacto no total de compras do cliente.""")
+st.write("""Para começar a entender um pouco mais sobre o conjunto de dados, 
+            podemos primeiramente analisar indicadores de estatística descritiva.
+            Abaixo é possível encontrar uma tabela com esses indicadores:""")
 
-df_treated_data = treated_data()
+# showing descriptive statistics metrics
+st.write(df_treated_data.describe())
 
 # defining the metrics for later use
 total_qty_ids = df_treated_data.count()[0]
@@ -27,23 +36,20 @@ median_clientes = round(df_treated_data['clientes'].describe()[5])
 mean_compras = round(df_treated_data['compras'].describe()[1])
 median_compras = round(df_treated_data['compras'].describe()[5])
 
-st.metric(label="qtd de ids", value=total_qty_ids)
+# AS LINHAS COMENTADAS ABAIXO COLOCAM MÉTRICAS EM COLUNAS E LINHAS, MAS SUBSTITUÍ PELA TABELA NO COMEÇO
+# # titles of the metrics to be presented
+# col1, col2 = st.columns(2)
+# col1.subheader("Métricas sobre o número de clientes:")
+# col2.subheader("Métricas sobre o número de compras:")
 
-# titles of the metrics to be presented
-col1, col2 = st.columns(2)
-col1.subheader("Métricas sobre o número de clientes:")
-col2.subheader("Métricas sobre o número de compras:")
+# # metric of client quantities
+# col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+# col1.metric(label="média de clientes", value=mean_clientes)
+# col2.metric(label="mediana de clientes", value=median_clientes)
 
-# metric of client quantities
-col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
-col1.metric(label="média de clientes", value=mean_clientes)
-col2.metric(label="mediana de clientes", value=median_clientes)
-
-# metric of purchase quantities
-col7.metric(label="média de compras", value=mean_compras)
-col8.metric(label="mediana de compras", value=median_compras)
-
-# st.write(df_treated_data.describe())
+# # metric of purchase quantities
+# col5.metric(label="média de compras", value=mean_compras)
+# col6.metric(label="mediana de compras", value=median_compras)
 
 st.subheader("Box Plot de Clientes - original do dataset, sem tratamento")
 # Client data box plot
@@ -60,7 +66,7 @@ fig.update_layout(
         pad=4
     ))
 st.plotly_chart(fig, theme=None)
-st.caption("""O box plot mostrado não possui qualquer tratamento, e representa os dados da forma que foram coletados.\n
+st.caption("""O box plot mostrado não possui qualquer tratamento, e representa os dados da forma que foram coletados.
            Passe o mouse sobre o gráfico e filtre a quantidade desejada.""")
 
 
